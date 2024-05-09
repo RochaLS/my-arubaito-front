cb7e31d4830fef6621c0985760aa567bc257c774
RochaLS <ld.rocha@hotmail.com>
2024-05-09 12:02:07 -0700

    add link that takes to home when logo is clicked

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,20 +6,25 @@ import {
   Avatar,
   Button,
   Icon,
+  Link,
 } from "@chakra-ui/react";
+
+import NextLink from "next/link";
 
 export function Navbar() {
   return (
     <Flex boxShadow="sm" bg="white" p={10} h={50} justify="space-between">
       <Center>
-        <Heading lineHeight="tall" size="lg">
-          <Highlight
-            query="Arubaito."
-            styles={{ px: "2", py: "1", rounded: "full", bg: "teal.100" }}
-          >
-            My Arubaito.
-          </Highlight>
-        </Heading>
+        <Link as={NextLink} href="/" _hover={{ textDecoration: "none" }}>
+          <Heading lineHeight="tall" size="lg">
+            <Highlight
+              query="Arubaito."
+              styles={{ px: "2", py: "1", rounded: "full", bg: "teal.100" }}
+            >
+              My Arubaito.
+            </Highlight>
+          </Heading>
+        </Link>
       </Center>
       <Center>
         <Button mr={10} variant="link" colorScheme="teal">
