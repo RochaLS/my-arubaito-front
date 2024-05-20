0d6b8139128512c4129630375f1bc9544d0f70f8
RochaLS <ld.rocha@hotmail.com>
2024-05-20 17:32:05 -0300

    implement validation to job form

--- src/app/components/JobForm.tsx
+++ src/app/components/JobForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Box,
   Heading,
@@ -11,6 +13,10 @@ import {
   FormHelperText,
 } from "@chakra-ui/react";
 
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { jobSchema } from "../util/validationSchemas";
+
 interface FieldData {
   hourlyRate: number;
   jobTitle: string;
@@ -18,7 +24,7 @@ interface FieldData {
 }
 
 interface JobFormProps {
-  onSubmit: (e: React.FormEvent) => void;
+  onSubmit: SubmitHandler<FieldValues>;
   data?: FieldData;
 }
 
@@ -29,6 +35,14 @@ export function JobForm({ data, onSubmit }: JobFormProps) {
   false indicating that data is null or undefined because null or undefined are falsy values.
   */
   let isEditForm = !!data;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(jobSchema), // Using zod to centralize my valitation rules
+  });
 
   return (
     <>
@@ -51,31 +65,51 @@ export function JobForm({ data, onSubmit }: JobFormProps) {
             <Heading m={[5, 10]} textAlign="center">
               {isEditForm ? "Edit job" : "Add job"}
             </Heading>
-            <FormControl>
-              <FormLabel>Hourly Rate</FormLabel>
-              <Input
-                focusBorderColor="teal.500"
-                size="lg"
-                mb={5}
-                placeholder="Hourly Rate"
-                type="number"
-                value={isEditForm ? data?.hourlyRate : ""}
-              />
-              <FormLabel>Job title / Company</FormLabel>
-              <Input
-                focusBorderColor="teal.500"
-                size="lg"
-                mb={5}
-                placeholder="Job title or company name"
-                type="text"
-                value={isEditForm ? data?.jobTitle : ""}
-              />
-              <Center>
-                <Button m={5} size="lg" colorScheme="teal" w="50%">
-                  Add
-                </Button>
-              </Center>
-            </FormControl>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl>
+                {errors.hourlyRate && (
+                  <p style={{ color: "red" }}>
+                    {errors.hourlyRate.message?.toString()}
+                  </p>
+                )}
+                <FormLabel>Hourly Rate</FormLabel>
+                <Input
+                  focusBorderColor="teal.500"
+                  size="lg"
+                  mb={5}
+                  placeholder="Hourly Rate"
+                  type="number"
+                  value={isEditForm ? data?.hourlyRate : ""}
+                  {...register("hourlyRate")}
+                />
+                {errors.jobTitle && (
+                  <p style={{ color: "red" }}>
+                    {errors.jobTitle.message?.toString()}
+                  </p>
+                )}
+                <FormLabel>Job title / Company</FormLabel>
+                <Input
+                  focusBorderColor="teal.500"
+                  size="lg"
+                  mb={5}
+                  placeholder="Job title or company name"
+                  type="text"
+                  value={isEditForm ? data?.jobTitle : ""}
+                  {...register("jobTitle")}
+                />
+                <Center>
+                  <Button
+                    m={5}
+                    size="lg"
+                    colorScheme="teal"
+                    w="50%"
+                    type="submit"
+                  >
+                    Add
+                  </Button>
+                </Center>
+              </FormControl>
+            </form>
           </Box>
         </Flex>
       </Box>
--- src/app/jobs/add/page.tsx
+++ src/app/jobs/add/page.tsx
@@ -1,15 +1,18 @@
+"use client";
+
 import { JobForm } from "@/app/components/JobForm";
 import { Navbar } from "@/app/components/Navbar";
+import { FieldValues, SubmitHandler } from "react-hook-form";
 
 export default function Page() {
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleJobSubmit: SubmitHandler<FieldValues> = (data) => {
+    // Handle form submission here
   };
 
   return (
     <>
       <Navbar />
-      <JobForm onSubmit={handleSubmit} />
+      <JobForm onSubmit={handleJobSubmit} />
     </>
   );
 }
