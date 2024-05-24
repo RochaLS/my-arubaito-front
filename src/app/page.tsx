1c85668c716525ba0eb9c7cca248847e6ebdea81
RochaLS <ld.rocha@hotmail.com>
2024-05-24 12:49:14 -0300

    display error message if shifts cannot be fetched

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Center, Heading, Box, Flex } from "@chakra-ui/react";
 import { ShiftBox } from "./components/ShiftBox";
 import { ListBox } from "./components/ListBox";
 import MyCalendar from "./components/Calendar";
+import { IoIosWarning } from "react-icons/io";
 
 export default async function Page() {
   const response = await fetch(
@@ -32,38 +33,52 @@ export default async function Page() {
     <>
       <Navbar />
       <Center>
-        <Box minW="80%">
-          <Heading m={[5, 10]} textAlign="center">
-            Dashboard
-          </Heading>
-          <Flex flexDir={["column", "row"]}>
-            <Box w={["100%", "90%", "90%", "70%"]} minH={300}>
-              <OverviewBox
-                data={{
-                  totalHours: data.totalHours,
-                  totalGrossPay: formatter.format(data.totalGrossPay),
-                  numOfShifts: data.shifts.length,
-                }}
-              />
-              <ShiftBox
-                nextShift={{
-                  shift: data.nextShift,
-                  totalHours: data.nextShiftTotalHours,
-                  moneyValue: formatter.format(data.nextShiftGrossPay),
-                }}
-              />
-            </Box>
-            <ListBox shifts={data.shifts} />
-          </Flex>
+        {!response.ok ? (
           <Box
-            boxShadow="md"
-            margin={10}
-            h={[0, 400, 500, 800]}
-            display={["none", "block"]}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDir="column"
+            mt={5}
           >
-            <MyCalendar shifts={data.shifts} />
+            <IoIosWarning size={100} color="teal" />
+            <Heading>Error fetching shifts, try again later.</Heading>
           </Box>
-        </Box>
+        ) : (
+          <Box minW="80%">
+            <Heading m={[5, 10]} textAlign="center">
+              Dashboard
+            </Heading>
+
+            <Flex flexDir={["column", "row"]}>
+              <Box w={["100%", "90%", "90%", "70%"]} minH={300}>
+                <OverviewBox
+                  data={{
+                    totalHours: data.totalHours,
+                    totalGrossPay: formatter.format(data.totalGrossPay),
+                    numOfShifts: data.shifts.length,
+                  }}
+                />
+                <ShiftBox
+                  nextShift={{
+                    shift: data.nextShift,
+                    totalHours: data.nextShiftTotalHours,
+                    moneyValue: formatter.format(data.nextShiftGrossPay),
+                  }}
+                />
+              </Box>
+              <ListBox shifts={data.shifts} />
+            </Flex>
+            <Box
+              boxShadow="md"
+              margin={10}
+              h={[0, 400, 500, 800]}
+              display={["none", "block"]}
+            >
+              <MyCalendar shifts={data.shifts} />
+            </Box>
+          </Box>
+        )}
       </Center>
     </>
   );
