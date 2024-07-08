600e74424af6887629dc1c398a94f34cbbf71d82
RochaLS <ld.rocha@hotmail.com>
2024-07-08 13:39:06 -0700

    remove unecessary code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,34 +26,6 @@ export default function RootLayout({
   console.log(siteUrl + ogImageUrl);
   return (
     <html lang="en">
-      <Head>
-        {/* Open Graph meta tags */}
-
-        <title>My Arubaito</title>
-        <meta name="title" content="My Arubaito" />
-        <meta
-          name="description"
-          content="Get income predictions for your part-time job."
-        />
-
-        {/* <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.myarubaito.com/" />
-        <meta property="og:title" content="My Arubaito" />
-        <meta
-          property="og:description"
-          content="Get income predictions for your part-time job."
-        />
-        <meta property="og:image" content={siteUrl + ogImageUrl} />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.myarubaito.com/" />
-        <meta property="twitter:title" content="My Arubaito" />
-        <meta
-          property="twitter:description"
-          content="Get income predictions for your part-time job."
-        />
-        <meta name="twitter:image" content={siteUrl + ogImageUrl} /> */}
-      </Head>
       <body>
         <Providers>{children}</Providers>
       </body>
